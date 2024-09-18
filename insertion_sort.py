7543a52a06f042dc1202d69f160fcdfbef33f3b4
Mike Smith <mike_smith@harvard.edu>
2024-09-18 09:24:35 -0400

    Fixes from code review Add a more general solution to `sorting_names.py` and change the way we observe the operation of `insertion_sort.py`

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,30 +1,61 @@
 ### chap12/insertion_sort.py
 import unsort
 
+# Terminal colors
+GRN = '\033[32m'   # green
+BLU = '\033[34m'   # blue
+BLK = '\033[0m'    # back to black
+
 def insertion_sort(s):
     """Sorts the unsorted sequence s in place"""
     for i in range(1, len(s)):
-        # print(f'Considering item at index {i}')
+        # observe_start(s, i)
+
         for j in range(i, 0, -1):
             if s[j-1] > s[j]:
                 # Swap the two items
                 s[j-1], s[j] = s[j], s[j-1]
-                # print(f'Swapped {j-1},{j}: {s}')
+                # observe_swap(s, i, j)
             else:
                 # When swaps are done, the ith element is where it belongs
                 break
 
+        # observe_done(s, i)
+
+def observe_start(s, i):
+    print(f'\nConsidering item with value {BLU}{s[i]}{BLK} at index {i}')
+    print(f'Start:         [', end='')
+    print(f'{GRN}{", ".join([str(item) for item in s[:i]])}', end='')       # sorted items
+    print(f', {BLK}{", ".join([str(item) for item in s[i:]])}', end='')
+    print(f']')
+
+def observe_swap(s, i, j):
+    print(f'Swapped {j-1:2},{j:2}: [', end='')
+    if j > 1:
+        print(f'{", ".join([str(item) for item in s[:j-1]])}, ', end='')
+    print(f'{BLU}{", ".join([str(item) for item in s[j-1:j+1]])}', end='')    # compared pair
+    if j + 1 != len(s):
+        print(f', {BLK}{", ".join([str(item) for item in s[j+1:]])}', end='')
+    print(f'{BLK}]')
+
+def observe_done(s, i):
+    print(f'Done:          [', end='')
+    print(f'{GRN}{", ".join([str(item) for item in s[:i+1]])}', end='')       # sorted items
+    if i + 1 != len(s):
+        print(f', {BLK}{", ".join([str(item) for item in s[i+1:]])}', end='')
+    print(f'{BLK}]')
+
 def main():
     a, c, w = unsort.unsort()
 
-    print(f'Sorting list A: {a}')
+    print(f'\nSorting list A: {a}')
     insertion_sort(a)
-    print(f'Sorting list C: {c}')
+    print(f'\nSorting list C: {c}')
     insertion_sort(c)
-    print(f'Sorting list W: {w}')
+    print(f'\nSorting list W: {w}')
     insertion_sort(w)
 
-    print(f'Sorted lists:')
+    print(f'\nSorted lists:')
     print(f'A = {a}\nC = {c}\nW = {w}')
 
 if __name__ == '__main__':
--- sorting_names.py
+++ sorting_names.py
@@ -1,20 +1,68 @@
 ### chap12/sorting_names.py
 from unsort import unsort
 
+###
+# Solution #1
+###
+
 # Some famous computer scientists and one who followed in
 # his mother's footsteps
-names = [
+cnames = [
     'Ada Lovelace',
     'Grace Hopper',
     'danah boyd',
     'Moses Liskov',
     'Barbara Liskov',
     'Anita Borg',
-    ]
+]
 
-def sort_key(item):
+def sort_key_c(item):
     '''Expects item to be a string of the form "first last"'''
     first, last = item.lower().split()
     return last + first
 
-print(sorted(names, key=sort_key))
+###
+# Solution #2
+###
+
+# Cast of the musical Hamilton
+hnames = [
+    'Lin-Manuel Miranda',
+    'Leslie Odom Jr.',
+    'Phillipa Soo',
+    'Renée Elise Goldsberry',
+    'Christopher Jackson',
+    'Daveed Diggs',
+    'Anthony Ramos',
+    'Okieriete Onaodowan',
+    'Brian d\'Arcy James',
+    'Jasmine Cephas Jones',
+]
+
+def sort_key_h(item):
+    '''Expects item to be a string of the form "first [middle ...] last"'''
+    name = item.lower().split()
+
+    # Grab the last name while avoiding common English name suffix. This code
+    # certainly doesn't handle every conceivable name suffix.
+    last = name[-1]
+    if last[-1] == '.' or last == 'II' or last == 'III' or last == 'IV':
+        # Skip suffixes like "Jr." and "Sr." and related with Roman numerals
+        last = name[-2]
+        # Remove this suffix from the `other` computation
+        name.pop()
+
+    others = ' '.join(name[:-1])
+    return last + others
+
+def main():
+    # Solution that handles names with only a first
+    # and last name.
+    print(sorted(cnames, key=sort_key_c))
+
+    # A more general solution for names with any number of
+    # components beyond one (i.e., no Madonna or Prince).
+    print(sorted(hnames, key=sort_key_h))
+
+if __name__ == '__main__':
+    main()
